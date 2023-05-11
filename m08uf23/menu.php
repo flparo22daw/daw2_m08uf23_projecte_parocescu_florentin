6110c6a6683d4f2635188ab253464d6512146d57
flparo22daw <113893492+flparo22daw@users.noreply.github.com>
2023-05-11 20:16:22 +0200

    projecte

---
+++ m08uf23/menu.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
+		<a href="crearusr.php">Crear usuario</a> <br>
+        <a href="verusrs.php">Ver usuarios</a><br>
+        <a href="modusr.php">Modificar usuarios</a><br>
+        <a href="delusr.php">Eliminar usuarios</a><br>
+        
+	</body>
+</html>
